3b8db9129326b02337e14f189df5e8b5ee96989c
Arannamoy-Mondal <arannamoymondal13@gmail.com>
2025-09-12 19:55:10 +0600

    upd:image upload

--- books/models.py
+++ books/models.py
@@ -9,7 +9,7 @@ class Book(models.Model):
     author=models.CharField(max_length=150)
     add_date=models.DateTimeField(auto_now_add=True)
     borrow_price=models.DecimalField(decimal_places=2,max_digits=12)
-    book_image=models.ImageField(upload_to="images",blank=True,null=True)
+    book_image=models.ImageField(upload_to="book_image",blank=True,null=True)
     added_by=models.CharField(max_length=150)
     category=models.ForeignKey(Category,on_delete=models.PROTECT,null=True)
     quantity=models.IntegerField(null=True)
