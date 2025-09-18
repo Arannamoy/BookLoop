98fd87cccd4f7e20a171849a69f46f8e221625ba
Arannamoy-Mondal <arannamoymondal13@gmail.com>
2025-09-19 04:45:43 +0600

    upd: Testing for onrender

--- BookLoop/settings.py
+++ BookLoop/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 ALLOWED_HOSTS = ["localhost","bookloop-s6np.onrender.com"]
-CSRF_TRUSTED_ORIGINS = ['https://bookloop-s6np.onrender.com/','http://localhost:8000']
+CSRF_TRUSTED_ORIGINS = ['https://bookloop-s6np.onrender.com','http://localhost:8000']
 
 # Application definition
 
