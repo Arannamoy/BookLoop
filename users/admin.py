3edc9c867c6106fef6f9411a3438ada082948f8e
Arannamoy-Mondal <arannamoymondal13@gmail.com>
2025-09-19 07:39:22 +0600

    upd:Not merged

--- users/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import User as UserModel
-# Register your models here.
-
-class UserAdmin(admin.ModelAdmin):
-    list_display=('user','balance')
-admin.site.register(UserModel,UserAdmin)
