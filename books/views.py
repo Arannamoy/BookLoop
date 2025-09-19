0091089ff2fa1db49680f0150eff2db996b981bb
Water-Pot <forh546@gmail.com>
2025-09-19 23:09:23 +0600

    trying to fixed get all book issue

--- books/views.py
+++ books/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 from .models import Book as BookModel
 # Create your views here.
-def getAllBook(r):
-    books=BookModel.objects.all()
-    return render(r,"books.html",{"books":books})
+# def getAllBook(r):
+    # books=BookModel.objects.all()
+    # return render(r,"books.html",{"books":books})
 
 def getSpecificBook(r,id):
      book=BookModel.objects.get(pk=id)
