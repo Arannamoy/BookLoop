7bd01331e7b91152ebc171d32bf6b0b3dea4db10
aeysha14 <aeyshatabassum07@gmail.com>
2025-09-20 00:29:28 +0600

    update books.urls

--- books/urls.py
+++ books/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import getSpecificBook
+from .views import getSpecificBook,borrowBook
 
 urlpatterns=[
     # path("all",getAllBook,name="home"),
-    path("<int:id>",getSpecificBook,name="specific_book")
+    path("<int:id>",getSpecificBook,name="specific_book"),
+    path("<int:id>",borrowBook,name="borrow_book")
+
 ]
