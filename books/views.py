3e612e7c3b5cbe3eee713ef5e0c01b2383e6bb2b
aeysha14 <aeyshatabassum07@gmail.com>
2025-09-20 00:27:00 +0600

    update books.views

--- books/views.py
+++ books/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Book as BookModel
 from borrow_records.models import Borrow_record as Borrow_recordModel
 from datetime import datetime
@@ -9,9 +9,16 @@ from datetime import datetime
     # return render(r,"books.html",{"books":books})
 
 def getSpecificBook(r,id):
-     book=BookModel.objects.get(pk=id)
-     return render(r,'book.html',{'book':book})
+    if r.user.is_authenticated:
+        book=BookModel.objects.get(pk=id)
+        return render(r,'book.html',{'book':book})
+    else:
+        return redirect('home')
+
 def borrowBook(r,id):
-    book=BookModel.objects.get(pk=id)
-    Borrow_recordModel.objects.create(user=r.user,book=book,borrow_date=datetime())
-    return render('borrow_record')
+    if r.user.is_authenticated:
+        book=BookModel.objects.get(pk=id)
+        Borrow_recordModel.objects.create(user=r.user,book=book,borrow_date=datetime())
+        return render('borrow_record')
+    else:
+        return redirect('home')
