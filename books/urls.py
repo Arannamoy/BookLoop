8943046be047202d571d9008293f3d7100f4caa3
Arannamoy-Mondal <arannamoymondal13@gmail.com>
2025-09-20 01:18:52 +0600

    upd:update borrow_records.models,books.models

--- books/urls.py
+++ books/urls.py
@@ -3,5 +3,6 @@ from .views import getSpecificBook,borrowBook
 
 urlpatterns=[
     # path("all",getAllBook,name="home"),
-    
+    path("<int:id>",getSpecificBook,name="specific_book"),
+    path("<int:id>",borrowBook,name="borrow_book")
 ]
--- books/views.py
+++ books/views.py
@@ -8,3 +8,17 @@ from datetime import datetime
     # books=BookModel.objects.all()
     # return render(r,"books.html",{"books":books})
 
+def getSpecificBook(r,id):
+    if r.user.is_authenticated:
+        book=BookModel.objects.get(pk=id)
+        return render(r,'book.html',{'book':book})
+    else:
+        return redirect('home')
+
+def borrowBook(r,id):
+    if r.user.is_authenticated:
+        book=BookModel.objects.get(pk=id)
+        Borrow_recordModel.objects.create(user=r.user,book=book,borrow_date=datetime())
+        return render('borrow_record')
+    else:
+        return redirect('home')
